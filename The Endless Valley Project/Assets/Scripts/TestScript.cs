ee5cba6b2816324fc3d48d5759de05e301e7ede1
doctorjaffa <79138930+doctorjaffa@users.noreply.github.com>
2021-03-09 10:57:03 +0000

    Created test script to learn C# basics.

---
+++ The Endless Valley Project/Assets/Scripts/TestScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public string myStartMessage;
+    public string myUpdateMessage;
+//    public int time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.LogError(myStartMessage);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.LogWarning(myUpdateMessage);
+//        time += 1;
+//        Debug.Log(time);
+    }
+}
