e700044b52f4f030f59b2db431b8d49d2e9dffa7
doctorjaffa <79138930+doctorjaffa@users.noreply.github.com>
2021-03-16 11:02:35 +0000

    Added player movement and jump.

---
+++ The Endless Valley Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Public variables
+    public float movementForce = 10f;
+    public float jumpForce = 1000f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Condition: When a player presses the D key...
+        if (Input.GetKey(KeyCode.D) == true)
+        {
+            // Action: Apply a force (move the player)
+            // Get the Rigidbody component off our player so we can use it
+            Rigidbody2D ourRigidbody = GetComponent<Rigidbody2D>();
+
+            // Add a force to the Rigidbody to move our player
+            ourRigidbody.AddForce(Vector2.right * movementForce);
+        }
+
+        // Condition: When a player presses the A key...
+        if (Input.GetKey(KeyCode.A) == true)
+        {
+            // Action: Apply a force (move the player)
+            // Get the Rigidbody component off our player so we can use it
+            Rigidbody2D ourRigidbody = GetComponent<Rigidbody2D>();
+
+            // Add a force to the Rigidbody to move our player
+            ourRigidbody.AddForce(Vector2.left * movementForce);
+        }
+
+        //Condition: When the player first presses space bar
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            //Action: Apply a force (push the player up)
+            // Get the Rigidbody component off our player so we can use it
+            Rigidbody2D ourRigidbody = GetComponent<Rigidbody2D>();
+
+            // Add a force to the Rigidbody to move our player
+            ourRigidbody.AddForce(Vector2.up * jumpForce);
+        }
+    }
+}
