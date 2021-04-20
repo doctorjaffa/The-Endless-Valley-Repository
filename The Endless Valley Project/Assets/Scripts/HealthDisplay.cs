f673ee01027d336562c83540c5fa70864020b20c
doctorjaffa <79138930+doctorjaffa@users.noreply.github.com>
2021-04-20 10:16:52 +0100

    Created Health Display with life system in place.

---
+++ The Endless Valley Project/Assets/Scripts/HealthDisplay.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class HealthDisplay : MonoBehaviour
+{
+
+    //This will be the Text component that displays the health value.
+    //Text = variable is in the form of a Text component.
+    Text healthValueDisplay;
+
+    //This will be the PlayerHealth component that contains information about the player's health.
+    //PlayerHealth = variable is in the form of a PlayerHealth component.
+    PlayerHealth player;
+
+    //Built in Unity function.
+    //Start is called before the first frame update.
+    void Start()
+    {
+        //Get a Text component from the game object this script is attached to.
+        //Store the Text component in healthValueDisplay variable.
+        healthValueDisplay = GetComponent<Text>();
+
+        //Search the scene for the object with PlayerHealth script attached.
+        //Store the PlayerHealth component from object in player variable.
+        player = FindObjectOfType<PlayerHealth>();
+    }
+
+    //Built in Unity function.
+    //Update is called once per frame.
+    void Update()
+    {
+        //Get current health value from the player using the GetHealth() function.
+        //Change the number to text using ToString().
+        //On the health value display Text component, set the text to be the number just retrieved.
+        healthValueDisplay.text = player.GetHealth().ToString();
+    }
+}
---
+++ The Endless Valley Project/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    //This will be the starting health for the player. 
+    //Public variable = shown in Unity editor and accessible from other scripts.
+    //Int = whole numbers. 
+    public int startingHealth;
+
+    //This will be the player's current health.
+    //Private variable = NOT shown in Unity or accessible from other scripts.
+    //Int = whole numbers.
+    private int currentHealth;
+
+    //Built in Unity function called when the object this script is attached to is created.
+    //Usually this is when the game starts unless the object is spawned in later.
+    //This happens BEFORE the Start() function.
+    //Usually used for initialisation.
+    void Awake()
+    {
+        //Initialise current health to be equal to starting health at beginning of game.
+        currentHealth = startingHealth;
+    }
+
+    //This function is NOT built into Unity. 
+    //It will only be called manually by own code. 
+    //It must be marked 'public' so other scripts can access it. 
+    //This function will change the health value of the player.
+    public void changeHealth(int changeAmount)
+    {
+        //Take current health, add the change amount and store the result back into current health.
+        currentHealth = currentHealth + changeAmount;
+
+        //Current health cannot go below zero or above starting health so special 
+        //function "Clamp" is used to keep it between 0 and the starting health.
+        currentHealth = Mathf.Clamp(currentHealth, 0, startingHealth);
+
+        //If health has dropped to 0, the player should die.
+        if (currentHealth == 0)
+        {
+            //Call the Kill function to kill the player.
+            Kill();
+
+        }
+
+    }
+
+    //This function is NOT built into Unity. 
+    //It will only be called manually by own code. 
+    //It must be marked 'public' so other scripts can access it. 
+    //This function will kill the player.
+    public void Kill()
+    {
+        //This will destroy the gameObject this script is attached to.
+        Destroy(gameObject);
+    }
+
+    //This simple function will let other scripts ask this one what the current health is.
+    //The function RETURNS an integer, meaning it gives a number back to the code that called it.
+    public int GetHealth()
+    {
+        return currentHealth;
+    }
+
+    //This simple function will let other scripts ask this one what the max health is.
+    //The function RETURNS an integer, meaning it gives a number back to the code that called it.
+    public int GetMaxHealth()
+    {
+        return startingHealth;
+    }
+}
