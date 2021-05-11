7010977ee3e8adc6cd8e9e8dbfaa99b222e146cf
doctorjaffa <79138930+doctorjaffa@users.noreply.github.com>
2021-05-11 11:48:06 +0100

    Fixed minor issues and exported project

---
+++ The Endless Valley Project/Assets/Scripts/InstaKill.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InstaKill : MonoBehaviour
+{
+
+    public string gameOverScene;
+    void OnTriggerEnter2D(Collider2D collisionData)
+    {
+        //Get the objected collided with.
+        Collider2D objectCollidedWith = collisionData;
+
+        //Get PlayerHealth attached to that object (if there is one).
+        PlayerHealth player = objectCollidedWith.GetComponent<PlayerHealth>();
+
+        //Check if a player health script was found on the object collided with. 
+        //This if statement is true if the player variable is NOT null (not empty).
+        if (player != null)
+        {
+            Destroy(gameObject);
+
+            //Load the game over scene.
+            SceneManager.LoadScene(gameOverScene);
+        }
+    }
+}
